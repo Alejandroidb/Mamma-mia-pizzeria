16bd26ab8e24d930b0791714a91bc18b475e5351
Alejandro <alejandroidb@gmail.com>
2024-05-26 15:46:12 -0400

    cambios menores

--- src/views/ShoppingCart.jsx
+++ src/views/ShoppingCart.jsx
@@ -7,7 +7,7 @@ const ShoppingCart = () => {
 
   // Agrupar pizzas por tipo
   const groupedPizzas = carrito.reduce((acc, pizza) => {
-    const existingPizza = acc.find(p => p.id === pizza.id);
+    const existingPizza = acc.find((p) => p.id === pizza.id);
     if (existingPizza) {
       existingPizza.quantity += 1;
       existingPizza.totalPrice += pizza.price;
@@ -18,7 +18,10 @@ const ShoppingCart = () => {
   }, []);
 
   // Calcular el precio total
-  const PrecioTotal = groupedPizzas.reduce((total, pizza) => total + pizza.totalPrice, 0);
+  const PrecioTotal = groupedPizzas.reduce(
+    (total, pizza) => total + pizza.totalPrice,
+    0
+  );
 
   return (
     <div className="container mt-3">
@@ -31,36 +34,30 @@ const ShoppingCart = () => {
             <div key={pizza.id} className="card mb-3">
               <div className="row g-0">
                 <div className="col-md-4">
-                  <img
-                    src={pizza.img}
-                    className="img"
-                    alt={pizza.name}
-                  />
+                  <img src={pizza.img} className="img" alt={pizza.name} />
                 </div>
                 <div className="col-md-8">
                   <div className="card-body">
-                  <div className="d-flex">
-                    <h5 className="card-title">{pizza.name}</h5>
-                    <div className="ms-auto d-flex">
-                    <p className="card-text me-1">
-                      <strong>Total:</strong> ${pizza.totalPrice}
-                    </p>
-                    <button
-                      className="btn btn-primary me-1"
-                      onClick={() => anadirCarrito(pizza)}
-                    >
-                      +
-                    </button>
-                    <p className="card-text">
-                      {pizza.quantity}
-                    </p>
-                    <button
-                      className="btn btn-danger ms-1"
-                      onClick={() => removerCarrito(pizza)}
-                    >
-                      -
-                    </button>
-                    </div>
+                    <div className="d-flex">
+                      <h5 className="card-title">{pizza.name}</h5>
+                      <div className="ms-auto d-flex">
+                        <p className="card-text me-1">
+                          <strong>Total:</strong> ${pizza.totalPrice}
+                        </p>
+                        <button
+                          className="btn btn-primary me-1"
+                          onClick={() => anadirCarrito(pizza)}
+                        >
+                          +
+                        </button>
+                        <p className="card-text">{pizza.quantity}</p>
+                        <button
+                          className="btn btn-danger ms-1"
+                          onClick={() => removerCarrito(pizza)}
+                        >
+                          -
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -70,13 +67,16 @@ const ShoppingCart = () => {
           <div className="mt-3">
             <h4>Total: ${PrecioTotal}</h4>
           </div>
-          
         </div>
       )}
       <div className="d-flex justify-content-between">
-          <NavLink to='/pagos' className='btn btn-success'>Ir a Pagar</NavLink>
-          <NavLink to='/' className='btn btn-danger ms-auto'>Volver al menú</NavLink>
-          </div>
+        <NavLink to="/pagos" className="btn btn-success">
+          Ir a Pagar
+        </NavLink>
+        <NavLink to="/" className="btn btn-danger ms-auto">
+          Volver al menú
+        </NavLink>
+      </div>
     </div>
   );
 };
